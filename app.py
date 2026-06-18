ed542f1eed4069e7d938b76f06d7fc1a7eacc6ed
DHEEPAN P <dheepand831@gmail.com>
2026-06-18 16:50:02 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -2,21 +2,24 @@ from flask import Flask, render_template, request
 import pickle
 import os
 
+# -----------------------------
 # Flask App
+# -----------------------------
+
 app = Flask(
     __name__,
-    template_folder="../templates",
-    static_folder="../static"
+    template_folder="templates",
+    static_folder="static"
 )
 
-# ----------------------------
-# Load Model and TF-IDF
-# ----------------------------
+# -----------------------------
+# Load Model
+# -----------------------------
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-model_path = os.path.join(BASE_DIR, "..", "models", "sentiment_model.pkl")
-tfidf_path = os.path.join(BASE_DIR, "..", "models", "tfidf_vectorizer.pkl")
+model_path = os.path.join(BASE_DIR, "models", "sentiment_model.pkl")
+tfidf_path = os.path.join(BASE_DIR, "models", "tfidf_vectorizer.pkl")
 
 with open(model_path, "rb") as f:
     model = pickle.load(f)
@@ -24,19 +27,17 @@ with open(model_path, "rb") as f:
 with open(tfidf_path, "rb") as f:
     tfidf = pickle.load(f)
 
-
-# ----------------------------
+# -----------------------------
 # Home Page
-# ----------------------------
+# -----------------------------
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-# ----------------------------
+# -----------------------------
 # Prediction
-# ----------------------------
+# -----------------------------
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -47,8 +48,8 @@ def predict():
 
     prediction = model.predict(review_vector)[0]
 
-    prob = model.predict_proba(review_vector)
-    confidence = round(max(prob[0]) * 100, 2)
+    probability = model.predict_proba(review_vector)
+    confidence = round(max(probability[0]) * 100, 2)
 
     if prediction == "Positive":
         emoji = "😊"
@@ -65,10 +66,9 @@ def predict():
         emoji=emoji
     )
 
-
-# ----------------------------
-# Run App
-# ----------------------------
+# -----------------------------
+# Run Flask
+# -----------------------------
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
